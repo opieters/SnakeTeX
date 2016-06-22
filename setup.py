d736394016817638033bfa69e5b98b72c0381b92
Olivier Pieters <me@olivierpieters.be>
2016-06-22 18:40:23 +0200

    Added PyPI setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""A setuptools based setup module for SnakeTeX"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path, remove, system
+from shutil import copyfile
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='SnakeTeX',
+    version='0.1',
+    description='A LaTeX template system for large and multi-user projects.',
+    install_requires=['Click', 'jinja2', 'PyYAML'],
+    entry_points={'console_scripts': ['stex=stex:main']},
+    package_dir={'snaketex': 'src'},
+    packages=find_packages(exclude=['example', 'docs', 'tests*']),
+    license='GPLv3',
+    author='Olivier Pieters',
+    author_email='me@olivierpieters.be',
+    url='https://github.com/opieters/SnakeTeX',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Text Processing :: Markup :: LaTeX',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=['LaTeX TeX XeTeX documents template system'],
+    package_data={'snaketex': ['*.stex']}
+)
